9eededce9926255550ea801d502dce06235d9f47
xyzuan <xyzuannihboss@gmail.com>
2022-02-07 11:49:31 +0000

    pine: xdroid: Inherit some gms stuff
    Signed-off-by: hsx02 <hsx02self@gmail.com>

--- xdroid_pine.mk
+++ xdroid_pine.mk
@@ -11,6 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit some common xdroidsp stuff.
 $(call inherit-product, vendor/xdroid/config/common.mk)
+XDROID_BOOT_DARK := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_SUPPORTS_QUICK_TAP := false
 
 # Inherit from pine device
 $(call inherit-product, device/xiaomi/pine/device.mk)
